844880de01423a66851d6d440b3ea919e2b3518d
Rinc Liu <i@Rinc.site>
2016-10-20 00:46:44 +0800

    :game_die:

---
+++ Base/Assets/CameraControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+	public float forceY = 0.123456789f;
+
+	private GameObject cube;
+	private Rigidbody rb;
+
+	void Start () {
+		cube = GameObject.FindWithTag ("CubeTag");
+		rb = cube.GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		rb.AddForce (new Vector3(0, forceY, 0));
+	}
+}
---
+++ Base/Assets/CubeControl.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeControl : MonoBehaviour {
+
+	private Transform mTransform;
+	private float screenWidth;
+
+	public float translateSpeedZ = 1.23f;
+	public float rotateSpeedX = 12f;
+	public float rotateSpeedY = 23f;
+
+	void Start () {
+		mTransform = this.transform;
+		screenWidth = Screen.width;
+		this.gameObject.GetComponent<Rigidbody> ().useGravity = false;
+	}
+
+	void Update () {
+		mTransform.Translate (new Vector3(0, 0, translateSpeedZ * Time.deltaTime));
+		mTransform.Rotate (new Vector3(0, rotateSpeedY * Time.deltaTime, 0));
+		//Handle touch events:
+		for (int i = 0; i < Input.touchCount; i++) {
+			Touch touch = Input.touches [i];
+			Vector2 pos = touch.position;
+			Debug.Log ("Touch at: " + pos.x + ", " + pos.y);
+			if (touch.phase == TouchPhase.Moved) {
+				mTransform.Rotate (new Vector3 (
+					pos.y < screenWidth / 2 ? -Time.deltaTime * rotateSpeedX : Time.deltaTime * rotateSpeedX,
+					0, 0));
+			}
+		}
+		//Handle Key events:
+		if (Application.platform == RuntimePlatform.Android) {
+			if (Input.GetKeyDown (KeyCode.Home)) {
+				Application.Quit ();
+			}
+		}
+	}
+}
