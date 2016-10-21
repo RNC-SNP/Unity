6942dd5fdfbdc05874a65bb1fe1508f176091b0e
Rinc Liu <i@Rinc.site>
2016-10-21 14:23:15 +0800

    Button.

---
+++ UGUI/Assets/ugui.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ugui : MonoBehaviour {
+
+	private Text t;
+
+	void Start() {
+		//Init Text:
+		t = this.gameObject.GetComponentInChildren<Text> ();
+		t.color = new Color (0f, 1f, 0f, 1f);
+		t.text = "Hello, Unity..";
+
+
+	}
+
+	public void onBtnClcik() {
+		Debug.Log ("Button is clicked..");
+		t.color = new Color (0f, 0f, 1f, 1f);
+		t.text = "Button is clicked..";
+	}
+}
