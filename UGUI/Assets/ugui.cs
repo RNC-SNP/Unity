ce6e614b0c14455ba1afe4f44db3423a2e5da474
Rinc Liu <i@Rinc.site>
2016-10-21 18:31:02 +0800

    Load Image from URL.

--- UGUI/Assets/ugui.cs
+++ UGUI/Assets/ugui.cs
@@ -4,20 +4,47 @@ using UnityEngine.UI;
 
 public class ugui : MonoBehaviour {
 
-	private Text t;
+	private string imgPath {
+		get {
+			return "https://Rinc.site/img/avatar.jpg";
+		}
+	}
+
+	private Text txt;
+	private Image img;
 
 	void Start() {
 		//Init Text:
-		t = this.gameObject.GetComponentInChildren<Text> ();
-		t.color = new Color (0f, 1f, 0f, 1f);
-		t.text = "Hello, Unity..";
+		txt = this.gameObject.GetComponentInChildren<Text> ();
+		txt.color = new Color (0f, 1f, 0f, 1f);
+		txt.text = "Hello, Unity..";
 
-
+		//Init Image:
+		img = this.gameObject.GetComponentInChildren<Image> ();
+		loadImageAsync (img, imgPath);
 	}
 
 	public void onBtnClcik() {
 		Debug.Log ("Button is clicked..");
-		t.color = new Color (0f, 0f, 1f, 1f);
-		t.text = "Button is clicked..";
+		txt.color = new Color (0f, 0f, 1f, 1f);
+		txt.text = "Button is clicked..";
+	}
+
+
+	private void loadImageAsync (Image img, string uri) {
+		//Use coroutine to load
+		StartCoroutine(loadImage (img, uri));
+	}
+
+	private IEnumerator loadImage (Image img, string uri) {
+		WWW www = new WWW(uri);
+		//Wait for the download
+		yield return www;
+		if (www.texture != null) {
+			Sprite sprite = Sprite.Create(www.texture, 
+				new Rect (0, 0, www.texture.width, www.texture.height), 
+				new Vector2 (0.5f, 0.5f));
+			img.sprite = sprite;
+		}
 	}
 }
